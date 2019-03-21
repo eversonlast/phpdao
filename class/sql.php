d7bd3a05d8257d6d9e57dd5a166524b6eadfc86c
eversonlast <eversonlast@yahoo.com.br>
2019-03-21 13:46:16 -0300

    inicianos um projeto

---
+++ class/sql.php
@@ -0,0 +1,40 @@
+<?php
+
+class sql extends PDO{
+	
+	private $conn;
+	
+	public function __construct(){
+		
+		$this->conn= new PDO("mysql:host=localhost; dbname=dbphp7", "root", "1234");
+	}
+	
+	private function setParams($statment, $parameters=array()){
+		
+		foreach($parameters as $key =>$values){
+			
+			$this->setParam($key, $values);
+		}
+		
+	}
+	
+	private function setParam($statment, $key, $values){
+		
+		$statment->bindParam($key, $values);
+	}
+	
+	public function query($rawQuery, $params=array()){
+		
+			$stmt=$this->conn->prepare($rawQuery);
+			$this->setParams($stmt, $params);
+			$stmt->execute();
+			return $stmt;
+		
+	}
+	
+	public function select ($rawQuery, $params=array()):array
+	{
+		$stmt=$this->query($rawQuery, $params);
+		return $stmt->fetchAll(PDO:: FETCH_ASSOC);
+	}
+}
